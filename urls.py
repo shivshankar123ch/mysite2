bac53c1561e884d27314ac42c40fbbda65cae7d7
shivshankar123ch <89379054+shivshankar123ch@users.noreply.github.com>
2023-09-14 19:24:30 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,36 +1,23 @@
-from django.urls import path
-from .views import RegisterAPI
-from . import views 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+"""new URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('api/register/', RegisterAPI.as_view(), name='register'),
-    path('api/login/', views.login, name='login'),#preectise
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-#perms user=1,2,3 list get
-    path('persional/', views.Personal_InformationList.as_view()),
-
-#ALL DETAILS GETLIST
-    path('all_detail/', views.alldetailList.as_view()),
-    
-#single get post
-    path('job/', views.Job_InformationList.as_view()),
-    path('employee/', views.Employee_Education_InformationList.as_view()),
-    path('services/', views.Employee_Old_Service_HistoryList.as_view()),
-    path('declration/', views.DeclarationList.as_view()),
-#crud prectise
-    path('declration/<int:pk>/', views.DeclarationDetail.as_view()),
-    path('persional/<int:pk>/', views.Personal_InformationDetail.as_view()),
-
-
-
-    path('api/persinal2/', views.persinal2, name='persinal2'),#preectise
-
+    path('admin/', admin.site.urls),
+    path('', include('first.urls')),
 
 ]
